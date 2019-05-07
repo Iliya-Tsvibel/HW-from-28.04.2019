a561121c81c598c8b08a16e049d5cfb1708faa85
Iliya-hacker <45923437+Iliya-hacker@users.noreply.github.com>
2019-05-07 23:55:50 +0300

    Add files via upload

---
+++ CustomerDB/CustomerDB/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (CustomersDBEntities customersDBEntities = new CustomersDBEntities())
+
+            {
+                List<Customer> customers = (from c in customersDBEntities.Customers
+                                 select c).ToList();
+                List<Customer> customersMS = customersDBEntities.Customers.ToList();
+                                            
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
