17cc469df4ef551d07f7f457870732aeabc32406
Iliya-hacker <45923437+Iliya-hacker@users.noreply.github.com>
2019-05-10 20:43:16 +0300

    Add files via upload

---
+++ CustomerDB_2/CustomerDB/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (CustomersDBEntities customersDBEntities = new CustomersDBEntities())
+
+            {
+                List<Customer> customers = (from c in customersDBEntities.Customers
+                                 select c).ToList();
+                List<Customer> customersMS = customersDBEntities.Customers.ToList();
+
+                customersDBEntities.Customers.ToList().ForEach(c => Console.WriteLine(JsonConvert.SerializeObject(c)));
+                
+                                            
+                
+
+            }
+        }
+    }
+}
